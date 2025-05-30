8decd512085ed1fa859151acf5535434a0c5d1ba
Sam Gee <samgee598@gmail.com>
2025-05-30 15:57:27 -0400

    Displayed content for first data point and added styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
-fetch("https://api.thedogapi.com/v1/images/search")
+const photoUrl = "https://api.thedogapi.com/v1/images/search";
+const breedUrl = "https://api.thedogapi.com/v1/breeds";
+
+//callback function for randomizeButton event listener
+function randomizePhoto(event) {
+    event.preventDefault();
+
+    //fetching API data
+    fetch(photoUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Request failed");
+            }
+            return response.json();
+        })
+
+        .then(randomData => {
+            const imageData = randomData;
+            console.log(imageData);
+
+            //displaying image in Dog Photos section
+            imageData.map(addImage => {
+                const photoSection = document.getElementById("dog-photos");
+                const image = document.createElement("img");
+                image.src = `${addImage.url}`;
+                photoSection.appendChild(image);
+            });
+        })
+        
+        .catch(error => {
+            console.error("An error occured:", error);
+        });
+}
+
+fetch(breedUrl)
     .then(response => {
         if (!response.ok) {
             throw new Error("Request failed");
@@ -6,28 +40,15 @@ fetch("https://api.thedogapi.com/v1/images/search")
         return response.json();
     })
 
-    .then(data => {
-        const imageData = data;
-        console.log(imageData);
+    .then(searchData => {
+        const breedData = searchData;
+        console.log(breedData);
     })
 
     .catch(error => {
         console.error("An error occured:", error);
     });
-
-fetch("https://api.thedogapi.com/v1/breeds")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Request failed");
-        }
-        return response.json();
-    })
-
-    .then(data => {
-        const imageData = data;
-        console.log(imageData);
-    })
-
-    .catch(error => {
-        console.error("An error occured:", error);
-    });
+   
+//event listener for randomize Button
+const randomizeButton = document.getElementsByName("randomize-dogs");
+randomizeButton[0].addEventListener("click", randomizePhoto);
